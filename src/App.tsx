8926b425abc3f58d352098761ad41d4f1e63278e
Yaaaaaaaama <mariamadide.diallo.1@gmail.com>
2024-05-17 21:50:50 +0200

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Menu from "./Components/Menu"; 
-import DisplayHighlight from "./Page/DisplayHighlight"; 
+import Menu from "./Components/Menu";
+import DisplayHighlight from "./Page/DisplayHighlight";
 import AdvancedSearch from "./Page/AdvancedSearch";
+import ObjectDetailPage from "./Page/ObjectDetailsPage";
 
 const router = createBrowserRouter([
   {
@@ -25,11 +25,15 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <DisplayHighlight />, 
+        element: <DisplayHighlight />,
       },
       {
         path: "/advanced-search",
         element: <AdvancedSearch />,
+      },
+      {
+        path: "/object/:objectId",
+        element: <ObjectDetailPage />,
       },
     ],
   },
@@ -37,9 +41,7 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   );
 }
 
